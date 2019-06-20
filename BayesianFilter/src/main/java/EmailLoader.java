87af6ae7d5cd781df0b2021f76cfc076eba6580a
sebastianVUCR <sebastian.vargas.99@gmail.com>
2019-06-20 15:12:40 -0600

    email parse
    codigo del lab para parse de email

--- BayesianFilter/src/main/java/EmailLoader.java
+++ BayesianFilter/src/main/java/EmailLoader.java
@@ -1,12 +1,24 @@
+import com.google.api.services.gmail.Gmail;
+import java.util.List;
+import com.google.api.services.gmail.model.ListMessagesResponse;
+import com.google.api.services.gmail.model.Message;
+
+
 public class EmailLoader {
 
 
 
     public EmailLoader() {
     }
-    public void getSpam()
+    public List<Email> getSpam(Gmail service)
     {
+        try {
+            ListMessagesResponse spamlist = service.users().messages().list("me").setQ("is:spam").execute();
+            List<Message> messages = spamlist.getMessages();
+        }
+        catch(Exception e){}
 
+        return null;
     }
     public void getNotSpam()
     {
